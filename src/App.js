01661de811c8f26365c12f5baeecc2d335cfa65d
Damia Dembicki <nikeid1235@gmail.com>
2021-03-19 13:06:40 +0100

    Created basic carousel component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Carousel from "./components/Carousel.jsx";
 
 const App = () => (
-  <>
-    <h1>Hello</h1>
-  </>
+  <Carousel />
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
