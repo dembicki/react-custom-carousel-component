8a81bcc238d7f89955befa6b43c27ff121fe1c59
dembicki <nikeid1235@gmail.com>
2021-03-20 20:35:02 +0100

    Updated README

--- README.md
+++ README.md
@@ -20,6 +20,38 @@ const App = () => (
 
 ```
 
+## Make image slider
+
+```JSX
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "./components/Carousel.jsx";
+
+const images = [{
+  name: "image1",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+},
+{
+  name: "image2",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+},
+{
+  name: "image3",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+}];
+
+const App = () => (
+  <>
+    <Carousel dots arrows >
+      {images?.map(({name, imageURL}) => (
+        <img key={index} src={imageURL} alt={name} />
+      ))}
+    </Carousel>
+  </>
+);
+
+```
+
 ## Parameters
 
 ### Show arrows (default: false)
--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom";
 import Carousel from "./components/Carousel.jsx";
 import "./App.css";
 
+const images = [{
+  name: "image1",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+},
+{
+  name: "image2",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+},
+{
+  name: "image3",
+  imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU",
+}];
+
 const App = () => (
   <>
-    <Carousel dots arrows>
-      <p>test</p>
-      <p>test2</p>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpPVyc4LETd6GWTNZWJCuiBWoEI3w5yHZeoA&usqp=CAU" alt="image" />
+    <Carousel dots arrows >
+      {images?.map(({name, imageURL}, index) => (
+        <img key={index} src={imageURL} alt={name} />
+      ))}
     </Carousel>
   </>
 );
